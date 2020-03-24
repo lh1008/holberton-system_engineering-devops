b7dce1619a7464e7da9ee86a415363c97f9d1532
lh008 <paintluis@gmail.com>
2020-03-23 23:40:34 -0500

    Executable warning using double quote, use single quote

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Puppet that kills a process
 exec { 'killmenow':
   path    => ['/usr/bin', '/bin', '/sbin', '/usr/local/bin'],
-  command => "pkill -f killmenow",
+  command => 'pkill -f killmenow',
 }
