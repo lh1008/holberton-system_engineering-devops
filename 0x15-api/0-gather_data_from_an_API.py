c0723251f8c758d021cab42bf660e7f9c19586c5
lh008 <paintluis@gmail.com>
2020-05-04 21:34:13 -0500

    Task 0 pep8 requirements

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,7 +12,7 @@ def main():
     todos = '{}/todos/?userId={}'.format(url, argv[1])
 
     # GET info from URLs
-    name =  urllib.request.urlopen(user)
+    name = urllib.request.urlopen(user)
     name_res = json.load(name)
     tasks = urllib.request.urlopen(todos)
     task_res = json.load(tasks)
@@ -21,14 +21,14 @@ def main():
     task_len = len(task_res)
     count = 0
     for v in task_res:
-        if v['completed'] == True:
+        if v['completed'] is True:
             count += 1
 
     # Prints
     print('Employee {} is done with tasks({}/{}):'.format(name_res['name'],
                                                           count, task_len))
     for t in task_res:
-        if t['completed'] == True:
+        if t['completed'] is True:
             print(t['title'])
 
 if __name__ == "__main__":
