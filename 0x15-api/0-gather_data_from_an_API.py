4fe66a2fd55082695c8ac50d829c0396f45f7abc
lh008 <paintluis@gmail.com>
2020-05-05 00:09:36 -0500

    Task 0, only requests with get()

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,27 +13,6 @@ def main():
     todos = '{}/todos/?userId={}'.format(url, argv[1])
 
     # GET info from URLs
-    name = urllib.request.urlopen(user)
-    name_res = json.load(name)
-    tasks = urllib.request.urlopen(todos)
-    task_res = json.load(tasks)
-    print(type(task_res))
-
-    # Lengths
-    task_len = len(task_res)
-    count = 0
-    for v in task_res:
-        if v['completed'] is True:
-            count += 1
-
-    # Prints
-    print('Employee {} is done with tasks({}/{}):'.format(name_res['name'],
-                                                          count, task_len))
-    for t in task_res:
-        if t['completed'] is True:
-            print('\t {}'.format(t['title']))
-
-    # GET info from URLs
     res = requests.get(user)
     info = res.json()
     tasks = requests.get(todos)
