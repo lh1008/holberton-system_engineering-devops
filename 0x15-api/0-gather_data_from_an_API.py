d9694a78d3a20eb392f5ef0c1a1858db514f4aae
lh008 <paintluis@gmail.com>
2020-05-05 00:08:00 -0500

    Both urllib and requests

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module that returns information about TODO list progress """
 import json
+import requests
 from sys import argv
 import urllib.request
 
@@ -16,6 +17,7 @@ def main():
     name_res = json.load(name)
     tasks = urllib.request.urlopen(todos)
     task_res = json.load(tasks)
+    print(type(task_res))
 
     # Lengths
     task_len = len(task_res)
@@ -30,6 +32,26 @@ def main():
     for t in task_res:
         if t['completed'] is True:
             print('\t {}'.format(t['title']))
+
+    # GET info from URLs
+    res = requests.get(user)
+    info = res.json()
+    tasks = requests.get(todos)
+    todo = tasks.json()
+
+    # Counter
+    counter = 0
+    for v in todo:
+        if v.get('completed') is True:
+            counter += 1
+
+    # Prints
+    print('Employee {} is done with tasks({}/{}):'.format(info.get('name'),
+                                                          counter, len(todo)))
+
+    for k in todo:
+        if k.get('completed') is True:
+            print('\t {}'.format(k.get('title')))
 
 if __name__ == "__main__":
     main()
