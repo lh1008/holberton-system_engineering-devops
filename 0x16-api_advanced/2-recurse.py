1cd1437ec5e468e2ee45365ffd13d4a79fd711a1
lh008 <paintluis@gmail.com>
2020-05-13 07:10:28 -0500

    Recurse it, Task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Module that recursively queries the Reddit API """
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """ Method that queries recursively hot titles of a given subreddit """
+
+    if after is None:
+        return []
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.\
+                                                       format(subreddit, after)
+
+    req = requests.get(url,
+                       headers={'User-Agent': 'me'},
+                       allow_redirects=False)
+
+    if req.status_code != 200:
+        return(print('None'))
+    else:
+        info = req.json()
+        for value in info.get('data').get('children'):
+            hot_list.append(value.get('data').get('title'))
+        after = info.get('data').get('after')
+        recursively = recurse(subreddit, [], after)
+        return hot_list + recursively
