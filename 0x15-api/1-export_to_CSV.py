7f7af83522a4992426adfeeab068b05dc900da44
lh008 <paintluis@gmail.com>
2020-05-19 07:29:35 -0500

    Export to CSV, Task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Module that exports data in the CSV format """
+import json
+import requests
+from sys import argv
+import csv
+import urllib.request
+
+
+def main():
+    """ Method that recordsall tasks that are owned by employee """
+    url = 'https://jsonplaceholder.typicode.com'
+    user = '{}/users/{}'.format(url, argv[1])
+    todos = '{}/todos/?userId={}'.format(url, argv[1])
+
+    # GET info from URLs
+    res = requests.get(user)
+    info = res.json()
+    tasks = requests.get(todos)
+    todo = tasks.json()
+
+    file_name = "{}.csv".format(argv[1])
+
+    with open(file_name, 'w', newline='') as file:
+        w = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            w.writerow([int(argv[1]), info.get('name'),
+                task.get('completed'), task.get('title')])
+
+if __name__ == "__main__":
+    main()
