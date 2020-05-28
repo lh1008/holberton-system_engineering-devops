1a4dbb3c46044f20062c8f2a7312c6691f957715
lh008 <paintluis@gmail.com>
2020-05-28 12:28:39 -0500

    Required first to execute change in file before restart nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,10 @@
 # Fix failed simulated HTTP requests using Apache ab
 exec { 'Debug failed requests':
-  command => 'sed -i \'s/15/15000/g\' /etc/default/nginx',
+  command => 'sed -i \'s/15/2000/g\' /etc/default/nginx',
   path    => ['/bin/'],
 }
 exec { 'nginx service restart':
   command => 'service nginx restart',
   path    => ['/usr/bin/', '/usr/sbin/',],
+  require => Exec['Debug failed requests'],
 }
