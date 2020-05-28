fb172f1742304ca3e413cdde4c7caeb64105c96c
lh008 <paintluis@gmail.com>
2020-05-28 11:58:13 -0500

    Changed path to provider shell

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # Fix failed simulated HTTP requests using Apache ab
 exec { 'Debug failed requests':
-  command => 'sed -i \'s/15/15000/g\' /etc/default/nginx',
-  path    => ['/bin']
+  command  => 'sed -i \'s/15/15000/g\' /etc/default/nginx',
+  provider => 'shell'
 }
 exec { 'nginx service restart':
-  command => 'service nginx restart',
-  path    => ['/usr/bin']
+  command  => 'service nginx restart',
+  provider => 'shell'
 }
