b6051142360882a27c18931af855b0de39216776
lh008 <paintluis@gmail.com>
2020-05-28 11:30:24 -0500

    puppent-lint indentation warnings

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # Fix failed simulated HTTP requests using Apache ab
 exec { 'Debug failed requests':
   command => 'sed -i "s/15/2000/g" /etc/default/nginx',
-  path => '/bin/'
+  path    => '/bin/'
 }
 exec { 'nginx service restart':
   command => 'sudo service nginx restart',
-  path => ['/usr/bin']
+  path    => ['/usr/bin']
 }
