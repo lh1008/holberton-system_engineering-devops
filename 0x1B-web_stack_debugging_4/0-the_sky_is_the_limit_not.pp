755786f21760049977a5bdd5f12175fa11af0b44
lh008 <paintluis@gmail.com>
2020-05-28 12:16:20 -0500

    Debugging puppet-lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # Fix failed simulated HTTP requests using Apache ab
 exec { 'Debug failed requests':
-  command  => 'sed -i \'s/15/15000/g\' /etc/default/nginx',
-  path => ['/bin/'],
+  command => 'sed -i \'s/15/15000/g\' /etc/default/nginx',
+  path    => ['/bin/'],
 }
 exec { 'nginx service restart':
-  command  => 'service nginx restart',
-  path => ['/usr/bin/', '/usr/sbin/',],
+  command => 'service nginx restart',
+  path    => ['/usr/bin/', '/usr/sbin/',],
 }
