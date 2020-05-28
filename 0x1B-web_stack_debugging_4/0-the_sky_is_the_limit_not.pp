1b8b8d7d16f18b93bdb73a6fb3d64f355e3fd47e
lh008 <paintluis@gmail.com>
2020-05-28 12:14:56 -0500

    Fixed again, and again

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # Fix failed simulated HTTP requests using Apache ab
 exec { 'Debug failed requests':
   command  => 'sed -i \'s/15/15000/g\' /etc/default/nginx',
-  provider => 'shell'
+  path => ['/bin/'],
 }
 exec { 'nginx service restart':
   command  => 'service nginx restart',
-  provider => 'shell'
+  path => ['/usr/bin/', '/usr/sbin/',],
 }
